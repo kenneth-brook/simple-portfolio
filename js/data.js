57d74e353966e595da82cc0ecab02988cd6892c0
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-05 18:59:24 -0400

    dynamic cards added

---
+++ js/data.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    id: 1,
+    title: "Rick & Morty API",
+  },
+  {
+    id: 2,
+    title: "NASA photo of the day",
+  },
+  {
+    id: 3,
+    title: "another awsome app",
+  },
+  {
+    id: 4,
+    title: "and yet another awsome app",
+  },
+];
+
+const articles = document.querySelector(".articles");
+// create new article and append it to the parentNode
+data.forEach((item) => {
+  articles.appendChild(createArticles(item.title));
+});
+
+function createArticles(title) {
+  //Main div class article
+  const article = document.createElement("div");
+  article.classList.add("article");
+
+  //Title
+  const header = document.createElement("h2");
+
+  // Append child elements
+  article.appendChild(header);
+
+  // TextContent for the elements
+  header.textContent = title;
+
+  return article;
+}
