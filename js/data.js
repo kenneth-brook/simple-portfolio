ffc9e264cded357767c9f71d6f76ef99d0931541
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-09 23:26:58 -0400

    data sheet expanded

--- js/data.js
+++ js/data.js
@@ -2,32 +2,57 @@ const data = [
   {
     id: 1,
     title: "Rick & Morty API",
+    img: "./images/randm.png",
+    hublink: "",
+    weblink: "",
+    tech: [],
+    info: "",
   },
   {
     id: 2,
     title: "NASA photo of the day",
+    img: "",
+    hublink: "",
+    weblink: "",
+    tech: [],
+    info: "",
   },
   {
     id: 3,
     title: "another awsome app",
+    img: "",
+    hublink: "",
+    weblink: "",
+    tech: [],
+    info: "",
   },
   {
     id: 4,
     title: "and yet another awsome app",
+    img: "",
+    hublink: "",
+    weblink: "",
+    tech: [],
+    info: "",
   },
   {
     id: 5,
     title: "Niyon",
+    img: "",
+    hublink: "",
+    weblink: "",
+    tech: [],
+    info: "",
   },
 ];
 
 const articles = document.querySelector(".articles");
 // create new article and append it to the parentNode
 data.forEach((item) => {
-  articles.appendChild(createArticles(item.title));
+  articles.appendChild(createArticles(item.title, item.img));
 });
 
-function createArticles(title) {
+function createArticles(title, img) {
   //Main div class article
   const article = document.createElement("div");
   article.classList.add("article");
@@ -35,11 +60,17 @@ function createArticles(title) {
   //Title
   const header = document.createElement("h2");
 
+  //Image
+  const image = document.createElement("img");
+  image.classList.add("image");
+
   // Append child elements
   article.appendChild(header);
+  article.appendChild(image);
 
   // TextContent for the elements
   header.textContent = title;
+  image.src = img;
 
   return article;
 }
