e1da4557b308d1088ff7077d8360ccc4f26bfff3
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-09-30 16:43:45 -0400

    started skill bar

--- js/nav.js
+++ js/nav.js
@@ -4,11 +4,11 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("navbar").style.paddingTop = "20px";
     document.getElementById("logo").style.fontSize = "25px";
     document.getElementById("pic").style.width = "75px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.paddingTop = "50px";
     document.getElementById("logo").style.fontSize = "40px";
     document.getElementById("pic").style.width = "150px";
   }
